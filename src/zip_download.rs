d460af92fbe37d8e982893fadba1a09c8933920f
L. Wlcek <luca@it-rahn.de>
2026-05-05 18:22:07 +0200

    re-enable compression

--- src/zip_download.rs
+++ src/zip_download.rs
@@ -87,7 +87,8 @@ async fn add_file_to_zip(
     path: &Path,
     archive_name: &str,
 ) -> anyhow::Result<()> {
-    let entry = ZipEntryBuilder::new(archive_name.to_string().into(), Compression::Stored);
+    let entry = ZipEntryBuilder::new(archive_name.to_string().into(), Compression::Deflate)
+        .deflate_option(DeflateOption::Fast);
     let mut entry_writer = zip.write_entry_stream(entry).await?;
     let file = tokio::fs::File::open(path).await?;
     let mut reader = BufReader::with_capacity(STREAM_BUFFER_SIZE, file);
