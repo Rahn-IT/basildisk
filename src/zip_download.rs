220e3894f5e114c1fffb4dd68922cae3e30e340f
L. Wlcek <luca@it-rahn.de>
2026-05-05 18:30:54 +0200

    try out zstd

--- src/zip_download.rs
+++ src/zip_download.rs
@@ -87,8 +87,7 @@ async fn add_file_to_zip(
     path: &Path,
     archive_name: &str,
 ) -> anyhow::Result<()> {
-    let entry = ZipEntryBuilder::new(archive_name.to_string().into(), Compression::Deflate)
-        .deflate_option(DeflateOption::Other(1));
+    let entry = ZipEntryBuilder::new(archive_name.to_string().into(), Compression::Zstd);
     let mut entry_writer = zip.write_entry_stream(entry).await?;
     let file = tokio::fs::File::open(path).await?;
     let mut reader = BufReader::with_capacity(STREAM_BUFFER_SIZE, file);
